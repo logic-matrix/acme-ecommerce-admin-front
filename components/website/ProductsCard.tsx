341359e7ea2ea2f24e0aac51ca16d2f84d8bde2b
foisal-logicmatrix <foisal.ahmmed@logicmatrix.tech>
2025-05-22 19:55:59 +0600

    product card cursor style change

--- components/website/ProductsCard.tsx
+++ components/website/ProductsCard.tsx
@@ -40,8 +40,8 @@ export default function ProductsCard({
         <p className="font-bold">${product.price}</p>
       </div>
       <div className="flex gap-2 mt-2">
-        <Button className="w-[50%]">Add to Cart</Button>
-        <Button className="w-[47%]" variant="outline">
+        <Button className="w-[50%] cursor-pointer">Add to Cart</Button>
+        <Button className="w-[47%] cursor-pointer" variant="outline">
           Buy Now
         </Button>
       </div>
